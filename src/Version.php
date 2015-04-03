9baae82638f897addb57ff849834260f64ce1a4a
Zeljko Simic <zeljkosbgd@gmail.com>
2015-04-03 10:22:08 +0200

    first commit

---
+++ src/Version.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace G4\Version;
+
+class Version
+{
+
+    const DEFAULT_VERSION = '0.0.0';
+
+    /**
+     * @var string
+     */
+    private $versionNumber;
+
+    /**
+     * @var string
+     */
+    private $path;
+
+    public function __construct($path)
+    {
+        $this->path = $path;
+    }
+
+    public function getVersionNumber()
+    {
+        if(!isset($this->versionNumber)) {
+            $this->loadVersion();
+        }
+        return $this->versionNumber;
+    }
+
+    private function loadVersion()
+    {
+        if ($this->path) {
+            $rawData = file_get_contents($this->path);
+            if (!empty($rawData)) {
+                $data = json_decode($rawData, true);
+            }
+        }
+        $this->versionNumber = empty($data['version']) ? self::DEFAULT_VERSION : $data['version'];
+    }
+}
